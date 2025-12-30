24dfaaed3aee4190583fd05f15b7f3ad5ea22a4e
Surendra <uchasurendra96@gmail.com>
2025-12-30 11:27:48 +0530

    Update project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,39 @@
+// import Navbar from '../components/Navbar';
+// import Hero from '../components/Hero';
+// import Services from '../components/Services';
+// import Portfolio from '../components/Portfolio';
+// import About from '../components/About';
+// import Testimonials from '../components/Testimonials';
+// import Contact from '../components/Contact';
+// import Footer from '../components/Footer';
+
+// const Home = () => {
+//   return (
+//     <main className="min-h-screen bg-background">
+//       <Navbar />
+//       <Hero />
+//       <Services />
+//       <Portfolio />
+//       <Testimonials />
+//       <About />
+//       <Contact />
+//       <Footer />
+//     </main>
+//   );
+// };
+
+// export default Home;
+
+
+
+
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import Portfolio from '../components/Portfolio';
 import About from '../components/About';
 import Testimonials from '../components/Testimonials';
+import Clients from '../components/Clients';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 
@@ -15,6 +45,7 @@ const Home = () => {
       <Services />
       <Portfolio />
       <Testimonials />
+      <Clients />
       <About />
       <Contact />
       <Footer />
